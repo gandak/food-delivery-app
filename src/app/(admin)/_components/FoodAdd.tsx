051a21935f91a4801b9ec55cfb8c06e5951128d0
24LP3548 24LP3548 <24LP3548@G304-CC0228.local>
2025-03-12 13:04:34 +0800

    edit

--- src/app/(admin)/_components/FoodAdd.tsx
+++ src/app/(admin)/_components/FoodAdd.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -9,11 +9,67 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Plus } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+type Food = {
+  foodName: string;
+  price: number;
+  image: string;
+  ingredients: string;
+  category: {};
+};
+
+type FoodValue = {
+  foodName: string;
+  price: number;
+  image: string;
+  ingredients: string;
+};
 
 export const FoodAdd = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  const [food, setFood] = useState<Food>();
+  const [foodValues, setFoodValues] = useState<>();
+
+  // const;
+
+  const addFood = async () => {};
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -29,21 +85,31 @@ export const FoodAdd = () => {
           <DialogTitle>Add new Dish to Appetizers</DialogTitle>
         </DialogHeader>
         <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          <div className="flex gap-6">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="name" className="text-left">
+                <p className="font-semibold">Food Name</p>
+              </Label>
+              <Input id="name" placeholder="" className="col-span-3" />
+            </div>
+            <div className="">
+              <Label htmlFor="number" className="text-left">
+                <p className="font-semibold">Food Price</p>
+              </Label>
+              <Input id="name" className="col-span-3" />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="username" className="text-left">
+              <p className="font-semibold">Ingredients</p>
             </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
+            <Textarea placeholder="Type your ingredients here." />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit" className="px-4">
+            Save changes
+          </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
--- src/app/(admin)/foodMenu/page.tsx
+++ src/app/(admin)/foodMenu/page.tsx
@@ -7,6 +7,7 @@ import { FoodAdd } from "../_components/FoodAdd";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -18,6 +19,13 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Plus } from "lucide-react";
 
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+
 type categoryType = {
   categoryName: string;
   createdAt: Date;
@@ -27,101 +35,182 @@ type categoryType = {
 
 const Page = () => {
   const [categories, setCategories] = useState<[]>();
+  const [catValue, setCatValue] = useState<string>("");
+  const [isUpdated, setIsUpdated] = useState<boolean>(false);
+  const [editCategoryName, setEditCategoryName] = useState<boolean>(false);
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [saveId, setSaveId] = useState<string>("");
+
+  const getCategories = async () => {
+    const data = await fetch("http://localhost:4000/food-category");
+    const categories = await data.json();
+    setCategories(categories.allCategories);
+  };
 
   useEffect(() => {
-    const getCategories = async () => {
-      const data = await fetch("http://localhost:4000/food-category");
-      const categories = await data.json();
-      setCategories(categories.allCategories);
-    };
     getCategories();
-  }, []);
+  }, [isUpdated]);
 
-  const addCategory = async () => {
+  const addCategory = async (categoryName: string) => {
     const data = await fetch("http://localhost:4000/food-category", {
       method: "POST",
-      body: JSON.stringify({ categoryName: "turshilt" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ categoryName: `${categoryName}` }),
     });
+
+    if (data.status == 200) {
+      alert("Төрөл амжилттай нэмлээ");
+      setIsUpdated(!isUpdated);
+    } else {
+      const jsonData = await data.json();
+      if (jsonData.error.errorResponse.code == 11000)
+        alert("Төрлийн нэр давхцаж байна ");
+      else {
+        alert("Төрөл нэмэхэд алдаа гарлаа");
+      }
+    }
+  };
+
+  const editCategory = async (id: string, categoryName: string) => {
+    try {
+      await fetch(`http://localhost:4000/food-category/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ categoryName }),
+      });
+    } catch (error) {
+      console.log("error", error);
+      alert("aldaa garlaa");
+    }
+    getCategories();
+  };
+
+  const deleteCategory = async (id: string) => {
+    try {
+      await fetch(`http://localhost:4000/food-category/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.log("error", error);
+      alert("aldaa garlaa");
+    }
+    getCategories();
+  };
+
+  const submitCat = () => {
+    if (isEdit) {
+      editCategory(saveId, catValue);
+    } else {
+      addCategory(catValue);
+    }
+    alert("Төрөл амжилттай засагдлаа");
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setCatValue(value);
+  };
+
+  const handleClickEdit = (id: string) => {
+    setEditCategoryName(true);
+    setIsEdit(true);
+    setSaveId(id);
+  };
+
+  const closeDiolog = () => {
+    setEditCategoryName(false);
   };
 
   return (
     <div className="w-screen bg-gray-100 p-8 flex flex-col gap-8">
       <div className="bg-white p-6 rounded-lg ">
         <h2 className="text-20px font-bold">Dishes category</h2>
-        <div className="flex gap-8 flex-wrap">
+        <div className="flex gap-8 flex-wrap items-center ">
           <ToggleGroup
             type="multiple"
             variant="outline"
-            className="flex flex-wrap justify-start py-2"
+            className="flex flex-wrap py-2  items-center"
           >
-            <ToggleGroupItem value="All" className="rounded-3xl mb-2">
-              <div className="text-nowrap px-4 flex gap-2">
+            <ToggleGroupItem value="all" className="rounded-3xl mb-2">
+              <div className="text-nowrap px-4 flex gap-2 items-center">
                 All Dishes{" "}
                 <div className="bg-black rounded-3xl h-[20px] flex items-center">
                   <p className="text-white px-3 text-[12px]">5</p>
                 </div>
               </div>
             </ToggleGroupItem>
+
             {categories?.map((category: categoryType, index: number) => (
-              <ToggleGroupItem
-                key={index}
-                value={category.categoryName}
-                className="rounded-3xl mb-2"
-              >
-                <div className="text-nowrap px-4 flex gap-2">
-                  {category.categoryName}{" "}
-                  <div className="bg-black rounded-3xl h-[20px] flex items-center">
-                    <p className="text-white px-3 text-[12px]">5</p>
-                  </div>
-                </div>
-              </ToggleGroupItem>
+              <ContextMenu>
+                <ContextMenuTrigger>
+                  <ToggleGroupItem
+                    key={index}
+                    value={category.categoryName}
+                    className="rounded-3xl mb-2 "
+                  >
+                    <div className="text-nowrap px-4 flex gap-2">
+                      {category.categoryName}{" "}
+                      <div className="bg-black rounded-3xl h-[20px] flex items-center">
+                        <p className="text-white px-3 text-[12px]">5</p>
+                      </div>
+                    </div>
+                  </ToggleGroupItem>
+                </ContextMenuTrigger>
+                <ContextMenuContent>
+                  <ContextMenuItem
+                    onClick={() => handleClickEdit(category._id)}
+                  >
+                    Edit
+                  </ContextMenuItem>
+                  <ContextMenuItem onClick={() => deleteCategory(category._id)}>
+                    Delete
+                  </ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
             ))}
 
-            <ToggleGroupItem value="All" className="rounded-3xl mb-2">
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button variant="outline" className="bg-transparent border-0">
-                    <div className="bg-[#EF4444] h-[40px] w-[40px] rounded-3xl flex justify-center items-center ">
-                      <Plus color="#ffffff" />
-                    </div>
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className="sm:max-w-[425px]">
-                  <DialogHeader>
-                    <DialogTitle>Edit profile</DialogTitle>
-                    <DialogDescription>
-                      Make changes to your profile here. Click save when you're
-                      done.
-                    </DialogDescription>
-                  </DialogHeader>
-                  <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="name" className="text-right">
-                        Name
-                      </Label>
-                      <Input
-                        id="name"
-                        value="Pedro Duarte"
-                        className="col-span-3"
-                      />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                      <Label htmlFor="username" className="text-right">
-                        Username
-                      </Label>
-                      <Input
-                        id="username"
-                        value="@peduarte"
-                        className="col-span-3"
-                      />
-                    </div>
+            <Dialog open={editCategoryName} onOpenChange={closeDiolog}>
+              <DialogTrigger asChild className="">
+                <div className="cursor-pointer bg-[#EF4444]  w-[36px] h-[36px] rounded-full flex justify-center items-center ">
+                  <Plus color="#ffffff" />
+                </div>
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>
+                    {isEdit ? "Edit category name" : "Add new category"}
+                  </DialogTitle>
+                  <DialogDescription></DialogDescription>
+                </DialogHeader>
+                <div className="gap-4 py-4">
+                  <div className="flex flex-col gap-2">
+                    <Label htmlFor="name" className="text-right">
+                      <p className="font-bold text-left">Category name</p>
+                    </Label>
+                    <Input
+                      id="name"
+                      onChange={handleInputChange}
+                      className="col-span-3"
+                      placeholder="Placeholder"
+                    />
                   </div>
-                  <DialogFooter>
-                    <Button type="submit">Save changes</Button>
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
-            </ToggleGroupItem>
+                </div>
+                <DialogFooter>
+                  <DialogClose asChild>
+                    <Button type="submit" className="px-4" onClick={submitCat}>
+                      <p>Add category</p>
+                    </Button>
+                  </DialogClose>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
           </ToggleGroup>
         </div>
       </div>
